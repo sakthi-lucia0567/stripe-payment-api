91bdddd7b3b0c4da2e050808e2ed2e66dba6857d
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2024-06-21 11:43:09 +0530

    feat: Error Handler setup

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 
 	// load the .env file
 	godotenv.Load(".env")
@@ -21,12 +23,34 @@ func main(){
 
 	router := chi.NewRouter()
 
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthcheck", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
-		Addr: ":" + port,
+		Addr:    ":" + port,
 	}
 
+	log.Printf("Server is running %v", port)
 	err := srv.ListenAndServe()
+
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	if err != nil {
 		log.Fatal(err)
